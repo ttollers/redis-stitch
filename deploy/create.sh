d4d7bc2dd77190aac9bdb439bb62b42ce350f620
James Chow <james@jc888.co.uk>
2015-11-26 16:48:25 +0000

    presentation service json

--- deploy/create.sh
+++ deploy/create.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#Point to the consul which matches the prefix
-sed -i.bak "s/ChronosConsul-dev/ChronosConsul-$ENVIRONMENT_SUFFIX/g" Dockerrun.aws.json
+if [ $S3_CONFIG_BUCKET ]; then aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_KEY development.js; fi
+export NODE_CONFIG=$(cat development.js | tr -d '\n ' | sed "s/module.exports=//g")
 
-eb init
-eb create presentation-service-$ENVIRONMENT_SUFFIX --cname "presentation-service-$ENVIRONMENT_SUFFIX" --single -k generator
+#Point to the consul which matches the prefix, and set NODE_CONFIG with json from the s3 bucket
+
+sed "s/ChronosConsul-dev/ChronosConsul-$ENVIRONMENT_SUFFIX/g" Dockerrun.aws.orig | \
+sed "s/\"{}\"/'$NODE_CONFIG'/g" > Dockerrun.aws.json
+cat Dockerrun.aws.json
+
+#eb init
+#eb create presentation-service-$ENVIRONMENT_SUFFIX --cname "presentation-service-$ENVIRONMENT_SUFFIX" --single -k generator
