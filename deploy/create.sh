22c46aac4b5d02263ac01538f77e08e9a8ab029f
James Chow <james@jc888.co.uk>
2015-11-27 10:15:10 +0000

    core-launcher incorporate starting presentation-service

--- deploy/create.sh
+++ deploy/create.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ $S3_CONFIG_BUCKET ]; then aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_KEY development.js; fi
-export NODE_CONFIG=$(cat development.js | tr -d '\n ' | sed "s/module.exports=//g")
+export NODE_CONFIG=$(cat development.js | tr -d '\n ' | sed -e 's/"/\\\\\"/g' | sed "s/module.exports=//g")
 
 #Point to the consul which matches the prefix, and set NODE_CONFIG with json from the s3 bucket
 
 sed "s/ChronosConsul-dev/ChronosConsul-$ENVIRONMENT_SUFFIX/g" Dockerrun.aws.orig | \
-sed "s/\"{}\"/'$NODE_CONFIG'/g" > Dockerrun.aws.json
+sed "s/{}/$NODE_CONFIG/g" > Dockerrun.aws.json
 cat Dockerrun.aws.json
 
-#eb init
-#eb create presentation-service-$ENVIRONMENT_SUFFIX --cname "presentation-service-$ENVIRONMENT_SUFFIX" --single -k generator
+eb init
+eb create presentation-service-$ENVIRONMENT_SUFFIX --cname "presentation-service-$ENVIRONMENT_SUFFIX" --single -k generator
---
+++ deploy/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "redis": {
+        "host": "chronos-presentation.9u29sh.0001.euw1.cache.amazonaws.com",
+        "port": "6379"
+    }
+}
