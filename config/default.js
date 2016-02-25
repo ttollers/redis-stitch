30104b67d07a16b80a7ab1acd56e56c014e2c5a2
Jeff Downie <jeff.downie@trinitymirror.com>
2016-02-25 11:29:04 +0000

    CRON-478 fixing bugs and adding logging.

--- config/default.js
+++ config/default.js
@@ -1,9 +1,14 @@
 module.exports = {
     "redis": {
-        "host": '127.0.0.1',
+        "host": 'redis',
         "port": 6379
     },
     "server": {
         "port": 8080
-    }
-}
+    },
+    "allowedMethods": [
+        'GET',
+        'PUT',
+        'DELETE'
+    ]
+};
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-/**
- * Created by DayoAdeyemi on 06/11/2015.
- */
+var R = require('ramda');
 var restify = require('restify');
 var db = require('./lib/db');
 var config = require('config');
@@ -8,11 +6,16 @@ var logger = require('winston');
 
 logger.info('Config : %j', config, {});
 
+function translateAPIMethodName(APIname) {
+    return APIname.toUpperCase() === 'DEL' ? 'DELETE' : APIname.toUpperCase();
+}
+
 function useAPI(prefix, server) {
     var api = require('./lib/' + prefix);
     for (var method in api) {
-        if (api.hasOwnProperty(method) && config.allowedMethods.hasOwnProperty(method)) {
+        if (api.hasOwnProperty(method) && R.contains(translateAPIMethodName(method), config.allowedMethods)) {
             server[method](new RegExp('\/' + prefix + '\/.+'), api[method]);
+            logger.info('Adding method '+ translateAPIMethodName(method) + ' on endpoint /' + prefix + '/');
         }
     }
 }
