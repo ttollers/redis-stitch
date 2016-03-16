1feac1ffbf6f7a13d7d768abfd6ad1029c97688a
ttollers <tom.tollinton@trinitymirror.com>
2016-03-16 15:53:20 +0000

    Formatting improvements

--- sdk.js
+++ sdk.js
@@ -5,12 +5,12 @@ var hl = require('highland');
 var stringify = require("./lib/stringify");
 
 
-module.exports = function(presentationServiceUrl) {
+module.exports = function (presentationServiceUrl) {
     var request;
     var getAndSetDb = {};
 
     if (R.isNil(presentationServiceUrl)) { // Test code
-        var url = "";
+        presentationServiceUrl = "";
         var rewire = require('rewire');
         var supertest = require('supertest');
         var restify = require('restify');
@@ -24,65 +24,58 @@ module.exports = function(presentationServiceUrl) {
         app.del(/.*/, v1.del);
 
         getAndSetDb = {
-            get: function() {
+            get: function () {
                 return db.store;
             },
-            set: function(_db) {
+            set: function (_db) {
                 db.store = _db;
             }
         };
     }
     else { // Live code
-        var url = presentationServiceUrl;
         request = require('superagent');
     }
 
     var returnObj = {
-        put: put(request, url),
-        putObject: putObject(request, url),
-        del: del(request, url),
-        add: add(request, url),
-        rem: rem(request, url),
-        get: get(request, url)
+        put: put(request, presentationServiceUrl),
+        putObject: putObject(request, presentationServiceUrl),
+        del: hl.wrapCallback((key, cb) => {
+            request
+                .del(presentationServiceUrl + key)
+                .end(catchRestErr(cb))
+        }),
+        add: hl.wrapCallback((key, score, value, cb) => {
+            request
+                .put(presentationServiceUrl + key)
+                .query({score: score})
+                .send(value)
+                .end(catchRestErr(cb))
+        }),
+        rem: hl.wrapCallback((key, value, cb) => {
+            request
+                .del(presentationServiceUrl + key)
+                .query({value: value})
+                .end(catchRestErr(cb))
+        }),
+        get: hl.wrapCallback((key, cb) => {
+            request
+                .get(presentationServiceUrl + key)
+                .end(catchRestErr(cb))
+        })
     };
 
     return Object.defineProperty(returnObj, "db", getAndSetDb);
 };
 
-var put = (request, presentationServiceUrl) => hl.wrapCallback(function (key, value, cb) {
+var put = (request, psUrl) => hl.wrapCallback((key, value, cb) => {
     request
-        .put(presentationServiceUrl + key)
+        .put(psUrl + key)
         .send(value)
         .end(catchRestErr(cb))
 });
 
-var putObject = R.curry((request, presentationServiceUrl, key, value) => {
-    return put(request, presentationServiceUrl)(key, stringify(value));
-});
-
-var add = (request, presentationServiceUrl) => hl.wrapCallback(function (key, score, value, cb) {
-    request
-        .put(presentationServiceUrl + key)
-        .query({score: score})
-        .send(value)
-        .end(catchRestErr(cb))
-});
-
-var rem = (request, presentationServiceUrl) => hl.wrapCallback(function (key, value, cb) {
-    request
-        .del(presentationServiceUrl + key)
-        .query({value: value})
-        .end(catchRestErr(cb))
-});
-var get = (request, presentationServiceUrl) => hl.wrapCallback(function (key, cb) {
-    request
-        .get(presentationServiceUrl + key)
-        .end(catchRestErr(cb))
-});
-var del = (request, presentationServiceUrl) => hl.wrapCallback(function (key, cb) {
-    request
-        .del(presentationServiceUrl + key)
-        .end(catchRestErr(cb))
+var putObject = R.curry((request, psUrl, key, value) => {
+    return put(request, psUrl)(key, stringify(value));
 });
 
 var catchRestErr = function (cb) {
