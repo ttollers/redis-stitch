96608f81ddc21f30d1daea1788b64e9cee8fd36e
Jeff Downie <jeff.downie@trinitymirror.com>
2016-04-07 16:21:58 +0100

    CRON-589 moving publish stuff higher up.

--- publishModule.sh
+++ publishModule.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -e
+
+#presentation-service-server upload
+cd server
 
 #get version
 export SERVER_VERSION=$(cat package.json | jq -r '.version')
@@ -6,6 +10,13 @@ export SERVER_VERSION=$(cat package.json | jq -r '.version')
 #build & push docker image
 docker build -t trinitymirror/presentation-service:$SERVER_VERSION .
 docker push trinitymirror/presentation-service:$SERVER_VERSION
+
+#publish latest version to npm
+npm publish
+
+
+#presentation-service-server upload
+cd ../js-sdk
 
 #publish latest version to npm
 npm publish
